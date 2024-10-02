b11c07d294652a10c7875537bf9d2643b0bbe2d4
Dominik Heinecke <dominikheinecke+github@icloud.com>
2024-10-02 21:23:05 +0200

    Projektdateien hinzufügen.

---
+++ 17-LetterCombinations/17-LetterCombinations/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _17_LetterCombinations
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+
+			Console.WriteLine();
+			Program program = new Program();
+			IList<string> result = program.LetterCombinations("23");
+
+			foreach (string item in result)
+			{
+				Console.WriteLine(item);
+			}
+		}
+		public IList<string> LetterCombinations(string digits)
+		{
+			List<String> ans = new List<string>();
+			if (digits.Length == 0)
+			{
+				return new List<string>();
+			}
+
+			int i = 0;
+
+			string output = "";
+
+			string[] mapping = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+
+			solve(digits, i, output, ans, mapping);
+			return ans;
+		}
+
+		private void solve(string digits, int i, string output, List<string> ans, string[] mapping)
+		{
+
+			if (i >= digits.Length)
+			{
+				ans.Add(output);
+				return;
+			}
+
+			int num = digits[i] - '0';
+			string letters = mapping[num];
+
+			for (int j = 0; j < letters.Length; j++)
+			{
+				solve(digits, i + 1, output + letters[j], ans, mapping);
+
+			}
+		}
+	}
+}
---
+++ 22-GenerateParantheses/22-GenerateParantheses/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _22_GenerateParantheses
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+
+			Solution solution = new Solution();
+			solution.GenerateParenthesis(1);
+		}
+	}
+
+	public class Solution
+	{
+		public IList<string> GenerateParenthesis(int n)
+		{
+			IList<string> result = new List<string>();
+			Recurse(result, 0, 0, "", n);
+			Console.WriteLine(result[0]);
+
+			return result;
+		}
+
+		public void Recurse(IList<string> result, int left, int right, String s, int n)
+		{
+			if (s.Length == n * 2)
+			{
+				result.Add(s);
+				return;
+			}
+
+			if (left < n)
+			{
+				Recurse(result, left + 1, right, s + "(", n);
+			}
+			if (right < left)
+			{
+				Recurse(result, left, right + 1, s + ")", n);
+			}
+		}
+	}
+}
