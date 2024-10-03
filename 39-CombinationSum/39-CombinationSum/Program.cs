f3fbadf0dfe83450d64c2eff18325f13e7f7fd2c
Dominik Heinecke <dominikheinecke+github@icloud.com>
2024-10-03 21:29:29 +0200

    39-CombinationSum

---
+++ 39-CombinationSum/39-CombinationSum/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _39_CombinationSum
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+
+			Solution s = new Solution();
+
+			s.CombinationSum(new int[] { 2, 3, 6, 7 }, 7);
+		}
+	}
+
+	public class Solution
+	{
+		public IList<IList<int>> CombinationSum(int[] candidates, int target)
+		{
+
+
+			IList<IList<int>> solution = new List<IList<int>>();
+			Array.Sort(candidates);
+			backtrack(solution, new List<int>(), candidates, target, 0);
+			return solution;
+		}
+
+		public void backtrack(IList<IList<int>> solution, IList<int> tempList, int[] num, int target, int start)
+		{
+			if (target < 0)
+			{
+				return;
+			}
+			else if (target == 0)
+			{
+				solution.Add(new List<int>(tempList));
+				return;
+			}
+			else
+			{
+				for (int i = start; i < num.Length; i++)
+				{
+					tempList.Add(num[i]);
+					backtrack(solution, tempList, num, target - num[i], i);
+					tempList.RemoveAt(tempList.Count - 1);
+				}
+			}
+
+		}
+	}
+}
