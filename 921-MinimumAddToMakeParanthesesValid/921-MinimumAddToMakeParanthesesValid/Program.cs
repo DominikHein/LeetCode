d5c60ead916885c3475d5bc5c92931accdae4cf4
Dominik Heinecke <dominikheinecke+github@icloud.com>
2024-10-09 21:57:51 +0200

    921. Minimum Add to Make Parentheses Valid

---
+++ 921-MinimumAddToMakeParanthesesValid/921-MinimumAddToMakeParanthesesValid/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _921_MinimumAddToMakeParanthesesValid
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+
+			Solution solution = new Solution();
+
+			var sol = solution.MinAddToMakeValid("())");
+			Console.WriteLine(sol);
+		}
+	}
+
+	public class Solution
+	{
+		public int MinAddToMakeValid(string s)
+		{
+			var stackOpen = 0;
+			var stackClose = 0;
+
+			for (int i = 0; i < s.Length; i++)
+			{
+				if (s[i] == '(')
+				{
+					stackOpen++;
+				}
+				else
+				{
+					if (stackOpen > 0)
+					{
+						stackOpen--;
+					}
+					else
+					{
+						stackClose++;
+					}
+				}
+			}
+			return stackOpen + stackClose;
+		}
+	}
+}
