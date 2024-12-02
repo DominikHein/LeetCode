46c9a336dbacb66cb46b28c0b6e0a8ad66c3b3cc
Dominik Heinecke <dominikheinecke+github@icloud.com>
2024-12-02 04:04:02 +0100

    217-ContainsDuplicate 242-ValidAnagram

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _217_ContainsDuplicate
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Solution solution = new Solution();
+			int[] nums = { 1, 2, 3, 1 };
+			Console.WriteLine(solution.ContainsDuplicate(nums));
+		}
+
+	}
+
+	public class Solution
+	{
+		public bool ContainsDuplicate(int[] nums)
+		{
+			Array.Sort(nums);
+			if (nums.Length == 0 || nums.Length == 1)
+			{
+				return false;
+			}
+
+			for (int i = 0; i < nums.Length - 1; i++)
+			{
+				if (nums[i] == nums[i + 1])
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+
+}
---
+++ 242-ValidAnagram/242-ValidAnagram/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace _242_ValidAnagram
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Solution solution = new Solution();
+			Console.WriteLine(solution.IsAnagram("anagram", "nagaram"));
+			Console.WriteLine(solution.IsAnagram2("anagram", "nagaram"));
+		}
+	}
+
+	public class Solution()
+	{
+		//Horrible Time Complexity
+		public bool IsAnagram(string s, string t)
+		{
+			if (s.Length != t.Length)
+			{
+				return false;
+
+			}
+
+			for (int i = 0; i < s.Length; i++)
+			{
+				for (int j = 0; j < t.Length; j++)
+				{
+					if (s[i] == t[j])
+					{
+						t = t.Remove(j, 1);
+						break;
+					}
+				}
+				if (t.Length == 0)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+
+		//Better Time Complexity
+		public bool IsAnagram2(string s, string t)
+		{
+			if (s.Length != t.Length)
+			{
+				return false;
+			}
+
+			char[] sArray = s.ToCharArray();
+			char[] tArray = t.ToCharArray();
+			Array.Sort(sArray);
+			Array.Sort(tArray);
+
+			if (sArray.SequenceEqual(tArray))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+
+		}
+	}
+
+}
