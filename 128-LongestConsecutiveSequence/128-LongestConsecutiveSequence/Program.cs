f77d84dfde1685d8cca43b92a6e37ce4ac8ef052
Dominik Heinecke <dominikheinecke+github@icloud.com>
2024-12-18 02:00:45 +0100

    128-LongestConsecutiveSequence 36-ValidSudoku

---
+++ 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _128_LongestConsecutiveSequence
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+			Solution solution = new Solution();
+			int[] nums = [2, 20, 4, 10, 3, 4, 5];
+			Console.WriteLine(solution.LongestConsecutive(nums));
+		}
+	}
+
+	public class Solution
+	{
+		public int LongestConsecutive(int[] nums)
+		{
+			HashSet<int> num_set = new HashSet<int>(nums);
+			int longest = 0;
+
+			foreach (int num in num_set)
+			{
+				if (!num_set.Contains(num - 1))
+				{
+					int length = 1;
+
+					while (num_set.Contains(num + length))
+					{
+						length++;
+					}
+
+					longest = Math.Max(longest, length);
+				}
+			}
+			return longest;
+		}
+	}
+}
---
+++ 36-ValidSudoku/36-ValidSudoku/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace _36_ValidSudoku
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+	public class Solution
+	{
+		public bool IsValidSudoku(char[][] board)
+		{
+
+			Dictionary<int, HashSet<char>> col = new Dictionary<int, HashSet<char>>();
+			Dictionary<int, HashSet<char>> row = new Dictionary<int, HashSet<char>>();
+			Dictionary<string, HashSet<char>> box = new Dictionary<string, HashSet<char>>(); // Key = (row/3 , col/3)
+
+			for (int r = 0; r < 9; r++)
+			{
+				for (int c = 0; c < 9; c++)
+				{
+					if (board[r][c] == '.')
+					{
+						continue;
+					}
+
+					string squarekey = (r / 3) + "," + (c / 3);
+
+
+					if (row.ContainsKey(r) && row[r].Contains(board[r][c]) ||
+						col.ContainsKey(c) && col[c].Contains(board[r][c]) ||
+						box.ContainsKey(squarekey) && box[squarekey].Contains(board[r][c]))
+					{
+						return false;
+					}
+
+					if (!row.ContainsKey(r))
+					{
+						row[r] = new HashSet<char>();
+					}
+
+					if (!col.ContainsKey(c))
+					{
+						col[c] = new HashSet<char>();
+					}
+
+					if (!box.ContainsKey(squarekey))
+					{
+						box[squarekey] = new HashSet<char>();
+					}
+
+					row[r].Add(board[r][c]);
+					col[c].Add(board[r][c]);
+					box[squarekey].Add(board[r][c]);
+
+				}
+			}
+			return true;
+		}
+
+	}
+}
