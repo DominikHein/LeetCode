742cb8a27ece36f1ee2a185264a6b9a596790713
Dominik Heinecke <dominikheinecke+github@icloud.com>
2024-12-28 04:00:11 +0100

    150-EvaluateReversePolishNotation 155-MinStack 20-ValidParantheses

---
+++ 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation/Program.cs
@@ -0,0 +1,94 @@
+﻿namespace _150_EvaluateReversePolishNotation
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Solution solution = new Solution();
+			string[] tokens = new string[] { "2", "1", "+", "3", "*" };
+			Console.WriteLine(solution.EvalRPN(tokens));
+		}
+	}
+
+	/*
+	You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+	Evaluate the expression. Return an integer that represents the value of the expression.
+
+	Note that:
+
+	The valid operators are '+', '-', '*', and '/'.
+	Each operand may be an integer or another expression.
+	The division between two integers always truncates toward zero.
+	There will not be any division by zero.
+	The input represents a valid arithmetic expression in a reverse polish notation.
+	The answer and all the intermediate calculations can be represented in a 32-bit integer.
+ 
+
+		Example 1:
+
+		Input: tokens = ["2","1","+","3","*"]
+		Output: 9
+		Explanation: ((2 + 1) * 3) = 9
+		Example 2:
+
+		Input: tokens = ["4","13","5","/","+"]
+		Output: 6
+		Explanation: (4 + (13 / 5)) = 6
+		Example 3:
+
+		Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+		Output: 22
+		Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+		= ((10 * (6 / (12 * -11))) + 17) + 5
+		= ((10 * (6 / -132)) + 17) + 5
+		= ((10 * 0) + 17) + 5
+		= (0 + 17) + 5
+		= 17 + 5
+		= 22 
+
+	 */
+
+	public class Solution
+	{
+		public int EvalRPN(string[] tokens)
+		{
+			Stack<int> stack = new Stack<int>();
+			foreach (var s in tokens) //iterate through the tokens
+			{
+				if (s == "+") //if the token is a + operator, pop the last two elements from the stack and add them together
+				{
+					var a = stack.Pop();
+					var b = stack.Pop();
+					stack.Push(a + b);
+				}
+				else if (s == "-") //if the token is a - operator, pop the last two elements from the stack and subtract them
+				{
+					var a = stack.Pop();
+					var b = stack.Pop();
+					stack.Push(b - a);
+				}
+				else if (s == "*") //if the token is a * operator, pop the last two elements from the stack and multiply them
+				{
+					var a = stack.Pop();
+					var b = stack.Pop();
+					stack.Push(a * b);
+				}
+				else if (s == "/") //if the token is a / operator, pop the last two elements from the stack and divide them
+				{
+					//Since the description says that the division between two integers always truncates toward zero, we can use integer division
+					//Also the description notes that there will be no division by zero , so we don't need to check for that
+					var a = stack.Pop();
+					var b = stack.Pop();
+					stack.Push(b / a);
+				}
+				else
+				{
+					stack.Push(int.Parse(s)); //if the token is a number, push it to the stack
+				}
+			}
+			return stack.Pop(); //return the last element in the stack
+
+		}
+	}
+}
---
+++ 20-ValidParentheses/20-ValidParentheses/Program.cs
@@ -0,0 +1,105 @@
+﻿namespace _20_ValidParentheses
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Solution solution = new Solution();
+			Console.WriteLine(solution.IsValid("()[]{}"));
+
+			StackSolution stackSolution = new StackSolution();
+			Console.WriteLine(stackSolution.IsValid("(()({[]}))"));
+		}
+	}
+
+	/*
+	Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+	An input string is valid if:
+
+	Open brackets must be closed by the same type of brackets.
+	Open brackets must be closed in the correct order.
+	Every close bracket has a corresponding open bracket of the same type.
+ 
+
+		Example 1:
+
+		Input: s = "()"
+
+		Output: true
+
+		Example 2:
+
+		Input: s = "()[]{}"
+
+		Output: true
+
+		Example 3:
+
+		Input: s = "(]"
+
+		Output: false
+
+		Example 4:
+
+		Input: s = "([])"
+
+		Output: true
+
+	 */
+}
+
+public class Solution //Brute force attempt
+{
+	public bool IsValid(string s)
+	{
+		while (s.Contains("()") || s.Contains("[]") || s.Contains("{}")) //While there are still pairs of brackets to remove
+		{
+			s = s.Replace("()", "").Replace("[]", "").Replace("{}", "");//Replace the pairs with an empty string
+		}
+		if (s.Length == 0) //If the string is empty, return true
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
+
+public class StackSolution
+{
+	public bool IsValid(string s)
+	{
+		Stack<char> stack = new Stack<char>();
+		Dictionary<char, char> bracketPairs = new Dictionary<char, char> //Dictionary to store the pairs of brackets
+		{
+			{')', '('},
+			{']', '['},
+			{'}', '{'}
+		};
+
+		foreach (char c in s)
+		{
+			if (bracketPairs.ContainsKey(c)) //If the character is a closing bracket
+			{
+				if (stack.Count > 0 && stack.Peek() == bracketPairs[c]) // If the stack is not empty and on the top of the stack is the corresponding opening bracket
+				{
+					stack.Pop(); //Removing the opening bracket from the stack 
+				}
+				else // If the opening bracket doesn't match the closing bracket
+				{
+					return false; //Return false
+				}
+			}
+			else //If the character is an opening bracket 
+			{
+				stack.Push(c); //add it to the stack
+			}
+		}
+		//If the stack is empty return true
+		return stack.Count == 0;
+	}
+}
+
