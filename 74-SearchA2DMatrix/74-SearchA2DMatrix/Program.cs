a44f982dbf0bdf0a2aeeae09f2a61d347d88efa8
Dominik Heinecke <dominikheinecke+github@icloud.com>
2025-03-25 23:02:47 +0100

    74-SearchA2DMatrix

---
+++ 74-SearchA2DMatrix/74-SearchA2DMatrix/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _74_SearchA2DMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.SearchMatrix(new int[][] { new int[] { 1, 3, 5, 7 }, new int[] { 10, 11, 16, 20 }, new int[] { 23, 30, 34, 60 } }, 3));
+
+        }
+    }
+
+    /*
+    You are given an m x n integer matrix matrix with the following two properties:
+
+    Each row is sorted in non-decreasing order.
+    The first integer of each row is greater than the last integer of the previous row.
+    Given an integer target, return true if target is in matrix or false otherwise.
+
+    Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+    Output: true
+   
+     */
+
+    public class Solution
+    {
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+            for (int r = 0; r < matrix.Length; r++)
+            {
+                for (int c = 0; c < matrix[r].Length; c++)
+                {
+                    if (matrix[r][c] == target)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
